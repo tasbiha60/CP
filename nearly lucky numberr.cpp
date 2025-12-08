8fc09c95e94052f484691b00086dc5b177970c1f
tasbiha60 <htjtasbi@gmail.com>
2025-12-08 23:24:11 +0600

    Create nearly lucky numberr.cpp
    nearly

---
+++ nearly lucky numberr.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n;
+    cin >> n;
+    int cnt = 0;
+    while(n){
+        int d = n % 10;
+        if(d == 4 || d == 7) cnt++;
+        n /= 10;
+    }
+    cout << ((cnt == 4 || cnt == 7) ? "YES" : "NO");
+}
