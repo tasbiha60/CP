52cfac6fdc2644f5ae03e957540c075ab7916914
tasbiha60 <htjtasbi@gmail.com>
2025-12-08 21:04:47 +0600

    Create petya and strings.cpp
    petya and strings

---
+++ petya and strings.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string a, b;
+    cin >> a >> b;
+    for(char &c : a) c = tolower(c);
+    for(char &c : b) c = tolower(c);
+
+    if(a < b) cout << -1;
+    else if(a > b) cout << 1;
+    else cout << 0;
+}
