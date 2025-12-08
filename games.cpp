b5b80f39f5d7b24add4363b666a44f150f0039a0
tasbiha60 <htjtasbi@gmail.com>
2025-12-08 23:30:18 +0600

    Create games.cpp
    games

---
+++ games.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<pair<int,int>> a(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> a[i].first >> a[i].second;
+    }
+
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(a[i].first == a[j].second)
+                cnt++;
+        }
+    }
+    cout << cnt;
+}
