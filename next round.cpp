5868dba3ffcccbf2f2c452cd791976f9c1e94b39
tasbiha60 <htjtasbi@gmail.com>
2025-12-08 20:55:00 +0600

    Create next round.cpp
    next round

---
+++ next round.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    int arr[100];
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] >= arr[k - 1] && arr[i] > 0)
+            count++;
+    }
+
+    cout << count;
+    return 0;
+}
