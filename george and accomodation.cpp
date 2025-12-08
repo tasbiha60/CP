44881efa6294e5da954dc8cda441394ffcd5542a
tasbiha60 <htjtasbi@gmail.com>
2025-12-08 22:35:45 +0600

    Create george and accomodation.cpp

---
+++ george and accomodation.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, p, q, cnt = 0;
+    cin >> n;
+    while(n--){
+        cin >> p >> q;
+        if(q - p >= 2) cnt++;
+    }
+    cout << cnt;
+}
